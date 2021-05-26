09fbf5ea37a001cfaff05cdcf89720e6fe38d297
linqueta <lincolnrodrs@gmail.com>
2021-05-26 13:39:29 -0300

    add certification service

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,6 +3,23 @@ import express from 'express';
 const routes = new express.Router();
 
 routes.post('/certifications', async (req, res) => {
+  const message = {
+    user: {
+      id: 1,
+      name: "Linqueta"
+    },
+    course: 'Kafka with Nodejs',
+    grade: 5
+  };
+
+  await req.producer.send({
+    topic: 'issue-certificate',
+    messages: [
+      {
+        value: JSON.stringify(message)
+      }
+    ],
+  });
 
   res.json({ ok: true })
 });
--- api/src/server.js
+++ api/src/server.js
@@ -6,15 +6,21 @@ const app = express();
 
 const kafka = new Kafka({
   clientId: 'api-producer',
-  brokers: ['kafka:9092']
+  brokers: ['localhost:9092']
 })
 
-const producer = kafka.producer()
+const producer = kafka.producer();
+
+app.use((req, res, next) => {
+  req.producer = producer;
+
+  return next();
+});
 
 app.use(routes);
 
 async function run() {
-  // await producer.connect()
+  await producer.connect();
 
   app.listen(3333);
 }
